afcf69584771257d8e74778f5da1f40b6bd2e93c
anantjain047 <jaina@alphavima.com>
2025-09-20 18:59:06 +0530

    redux setup and created basic global states

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -9,43 +9,55 @@ import {
   Tab,
 } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 function Navbar() {
+  // global state :
+  const isLogin = useSelector((state) => state.isLogin);
+
   const [value, setValue] = useState();
   return (
     <>
       <AppBar position="sticky">
         <Toolbar>
           <Typography>My Blog App</Typography>
-          <Box display={"flex"} marginLeft={"auto"} marginRight={"auto"}>
-            <Tabs
-              textColor="inherit"
-              value={value}
-              onChange={(e, val) => {
-                setValue(val);
-              }}
-            >
-              <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
-              <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs" />
-            </Tabs>
-          </Box>
+          {isLogin && (
+            <Box display={"flex"} marginLeft={"auto"} marginRight={"auto"}>
+              <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={(e, val) => {
+                  setValue(val);
+                }}
+              >
+                <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
+                <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs" />
+              </Tabs>
+            </Box>
+          )}
           <Box display={"flex"} marginLeft="auto">
-            <Button
-              sx={{ margin: 1, color: "white" }}
-              LinkComponent={Link}
-              to="/login"
-            >
-              Login
-            </Button>
-            <Button
-              sx={{ margin: 1, color: "white" }}
-              LinkComponent={Link}
-              to="/register"
-            >
-              Register
-            </Button>
-            <Button sx={{ margin: 1, color: "white" }} LinkComponent={Link}>
-              LogOut
-            </Button>
+            {!isLogin && (
+              <>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/login"
+                >
+                  Login
+                </Button>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/register"
+                >
+                  Register
+                </Button>
+              </>
+            )}
+            {isLogin && (
+              <Button sx={{ margin: 1, color: "white" }} LinkComponent={Link}>
+                LogOut
+              </Button>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
